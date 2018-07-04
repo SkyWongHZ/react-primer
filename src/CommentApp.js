db2d30e2c1263ab79b4712119f5a6cbd02bb41c8
SkyWongHZ <120217152skysky@sina.cn>
2018-07-05 00:09:35 +0800

    提交暂存文件

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -7,18 +7,30 @@ class CommentApp extends Component {
 	constructor(){
 		super()
 		this.state={
-			test:'hello  world'
+			comments:[],
 		}
 	}
-	handleSubmit=()=>{
-		alert('点击')
+	ss=()=>{
+		
+	}
+	handleSubmit=(params)=>{
+		if(!params)return 
+		if(!params.userName){ return alert('请输入用户名') }
+		if(!params.content){ return alert('请输入文本内容') }
+		const data= Object.assign({},params)
+		this.state.comments.push(data)
+		this.setState({
+			comments:this.state.comments
+		},()=>{
+			console.log('this.state.comments',this.state.comments)
+		})
 	}
 	render() {
+		console.log('this.state.test',this.state.test)
 		return (
-
 			<div className='wrapper'>
-				<CommentInput />
-				<CommentList  handleSubmit={this.handleSubmit} test={this.state.test}/>
+				<CommentInput handleSubmit={this.handleSubmit} />
+				<CommentList comments={this.state.comments} />
 			</div>
 
 		)
