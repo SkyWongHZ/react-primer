fecf8d5e42423567adfa09cf7ab0fbbcd75852fc
SkyWongHZ <120217152skysky@sina.cn>
2018-07-05 00:12:47 +0800

    add:CommentList页面

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Comment from './Comment'
 
 class CommentList extends Component {
+  
   render() {
     return (
-      <div>CommentList</div>
+      <div>{this.props.comments&&this.props.comments.map((comment, i) => {
+        return (
+          <Comment comment={comment} key={i}></Comment>
+        )
+      })}</div>
     )
   }
 }
-
 export default CommentList
